01895d1231434e60ec7071c00a127210ba627019
janhawk <janlawrenceabalos@gmail.com>
2020-10-22 11:57:37 +0200

    3 files modified, 2 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,15 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { FormsModule } from "@angular/forms";
 
-import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
-import { BlogComponent } from './blog.component';
+import { AppComponent } from "./app.component";
+import { HelloComponent } from "./hello.component";
+import { BlogComponent } from "./blog.component";
+import { LikeComponent } from "./like.component";
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule, ],
-  declarations: [ AppComponent, HelloComponent, BlogComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [BrowserModule, FormsModule],
+  declarations: [AppComponent, HelloComponent, BlogComponent, LikeComponent  ],
+  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {} 
---
+++ src/app/like.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input } from "@angular/core";
+
+@Component({
+  selector: "like-box",
+  templateUrl: "like.component.html",
+})
+export class LikeComponent {
+  numberOfLikes: number = 0;
+
+  likeButtonClick() {
+    this.numberOfLikes++;
+  }
+
+  dislikeButtonClick() {
+    this.numberOfLikes--;
+  }
+}
